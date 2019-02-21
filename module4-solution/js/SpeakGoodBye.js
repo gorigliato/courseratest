b663dfcc29165d6202e805d919d43be49120624a
Origliato <gorigliato@wiley.com>
2019-02-21 15:54:39 -0500

    Added assignment 4

---
+++ module4-solution/js/SpeakGoodBye.js
@@ -0,0 +1,18 @@
+(function (window) {
+
+        var speakWord = "Good Bye";
+
+        var byeSpeaker = {};
+        byeSpeaker.speak = function speak(name) {
+            console.log(speakWord + " " + name);
+        }
+    
+        function speak(name) {
+          console.log(speakWord + " " + name);
+        }
+
+    
+    window.byeSpeaker = byeSpeaker;   
+    
+    
+})(window);
